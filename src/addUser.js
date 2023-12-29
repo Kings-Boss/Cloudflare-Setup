b712e706e97bb381d13f05a111f0d20fa46d86b3
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2023-12-29 19:14:11 +0530

    Initial

---
+++ src/addUser.js
@@ -0,0 +1,21 @@
+async function addUser(update) {
+  const userInfo = {
+    userId: update.message.chat.id,
+    firstName: update.message.chat.firstName,
+    lastName: update.message.chat.lastName,
+    username: update.message.from.username || '',  // Handle optional username
+    isBot: update.message.from.isBot
+  };
+
+  const Allusers = await getKV("AllUsers");
+  let userIndex = Allusers.length;
+
+  if (!userInfo.firstName && !userInfo.lastName) {
+    userInfo.Name = `User${userIndex}`;
+  }
+  if (!Allusers.some(user => user.userId === userInfo.userId)) {
+    Allusers.push(userInfo);
+  }
+  
+  await KV.put("AllUsers", JSON.stringify(Allusers));
+}
