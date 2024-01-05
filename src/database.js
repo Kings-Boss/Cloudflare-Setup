6542e40fd5f99d1985c786b265277588664f21ae
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-05 05:28:47 +0000

    Database Changed

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 
 export async function putdb(path, data) {
-    const databaseUrl = `https://mybot-md2-default-rtdb.firebaseio.com/${path}.json`;
+    const databaseUrl = `https://contact-bot-0105-default-rtdb.firebaseio.com/${path}.json`;
 
         // Make a PUT request to the Firebase Realtime Database
         const response = await fetch(databaseUrl, {
@@ -12,7 +12,7 @@ export async function putdb(path, data) {
 
 
 export async function getdb(path) {
-    const databaseUrl = `https://mybot-md2-default-rtdb.firebaseio.com/${path}.json`;
+    const databaseUrl = `https://contact-bot-0105-default-rtdb.firebaseio.com/${path}.json`;
     const response = await fetch(databaseUrl);
     const data = await response.json();
     return data
