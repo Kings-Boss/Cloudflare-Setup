fa6230b684f83b726d635d95381ecd2fd64a961d
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-17 21:04:40 +0000

    fix3

--- src/fsub.js
+++ src/fsub.js
@@ -1,4 +1,5 @@
 import {getdb, putdb, deldb} from "./database.js"
+var telegramAuthToken = "1861934584:AAFoRVzflmY9dTRkMZT8E4e9FJN0apqgbyw";
 
 export async function isSub(userId) {
 
@@ -7,8 +8,8 @@ export async function isSub(userId) {
     const response = await fetch(data);
     const datajson = await response.json();
     if (datajson.result.status === 'left' || datajson.result.status === 'kicked') {
-        return false
-      } else {
-        return true
-      }
+      return false
+    } else {
+      return true
+    }
 }
