ab6903dbba002e93c7cb607721d508bd5b62460e
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-17 20:56:38 +0000

    token fixed

--- src/fsub.js
+++ src/fsub.js
@@ -3,7 +3,7 @@ import {getdb, putdb, deldb} from "./database.js"
 export async function isSub(userId) {
 
     const channel = await getdb('vars/channel');
-    const data = `https://api.telegram.org/bot${token}/getChatMember?chat_id=@${channel}&user_id=${userId}`;
+    const data = `https://api.telegram.org/bot${telegramAuthToken}/getChatMember?chat_id=@${channel}&user_id=${userId}`;
     const response = await fetch(data);
     const datajson = await response.json();
     if (datajson.result.status === 'left' || datajson.result.status === 'kicked') {
