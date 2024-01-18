d04fc58c0f0bfd089f81c9ab7fdc75e06dc67396
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-18 03:05:05 +0000

    ..

--- src/forceSub.js
+++ src/forceSub.js
@@ -5,5 +5,9 @@ export async function isSub(userId) {
     const data = `https://api.telegram.org/bot${telegramAuthToken}/getChatMember?chat_id=@${channel}&user_id=${userId}`;
     const response = await fetch(data);
     const datajson = await response.json();
-    return (datajson.result.status === 'left' || datajson.result.status === 'kicked')
+    if (datajson.result.status === 'left' || datajson.result.status === 'kicked') {
+        return true
+    } else {
+        return false
+    }
 }
