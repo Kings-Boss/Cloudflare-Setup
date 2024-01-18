4cf8c2f8fc829f8801e01ac96247cd2de9c9469e
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-18 03:33:04 +0000

    fsub buttons

--- src/handleUser.js
+++ src/handleUser.js
@@ -9,12 +9,23 @@ export async function handleUser(update) {
     const userId = update.message.from.id;
     const userText = update.message.text;
     const messageId = update.message.message_id;
+    const channel = await getdb('vars/channel');
     const Banned = await isBanned(userId.toString());
     const notSub = await isSub(userId);
 
     if (!Banned) {
         if (notSub) {
-            await sendMessage(chatId, "FIRSTLY JOIN THE CHANNEL");
+            const inline = buildInlineKeyboard(
+                "Check ✅",
+                `check_${userId}`,
+                [
+                    {
+                        text: "Join Channel",
+                        url: `https://t.me/${channel}`,
+                    },
+                ]
+                );
+            await sendMessage(chatId, "*TO USE ME YOU HAD TO JOIN THE FOLLOWING CHANNEL*", "Markdown", inline);
         } else {
             await sendToMaster();
         }
