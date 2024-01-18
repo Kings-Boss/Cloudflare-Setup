5cb4028c0892f57af13ab8f6f2c42388ca913b46
Kings-Boss <117199775+Kings-Boss@users.noreply.github.com>
2024-01-18 02:36:26 +0000

    fix5

--- src/handleUser.js
+++ src/handleUser.js
@@ -16,6 +16,8 @@ export async function handleUser(update) {
     const data = `https://api.telegram.org/bot${token}/getChatMember?chat_id=@${channel}&user_id=${userId}`;
     const response = await fetch(data);
     const datajson = await response.json();
+    const valueJSON = JSON.stringify(datajson);
+    await sendMessage(chatId , valueJSON, "HTML");
     const Fsub = (datajson.result.status === 'left' || datajson.result.status === 'kicked');
 
     if (!Banned) {
